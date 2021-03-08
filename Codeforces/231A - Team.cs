69cf12250465f2661d9f3c9f30289adf2422a81d
Sh4rpSh4d0w <Sh4rpSh4d0w@protonmail.com>
2021-03-08 16:01:12 +0100

    Solution for 2 problems from Codeforces

---
+++ Codeforces/231A - Team.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 231A_Team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string line;
+            n = Convert.ToInt32(Console.ReadLine());
+            int sumOfSolutions = 0;
+
+            while (n != 0)
+            {
+                line = Console.ReadLine();
+                string[] numbers = line.Split(' ');
+                int NumberOfSolutions = 0;
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (Convert.ToInt32(numbers[i]) != 0)
+                    {
+                        NumberOfSolutions++;
+                    }
+                    if (NumberOfSolutions > 1)
+                    {
+                        sumOfSolutions++;
+                        break;
+                    }
+
+                }
+                n--;
+            }
+            Console.WriteLine(sumOfSolutions);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Codeforces/71A - Way Too Long Words.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 71A_Way_Too_Long_Words
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int n;
+        string line;
+        n = Convert.ToInt32(Console.ReadLine());
+        int sumOfSolutions;
+
+        while (n != 0)
+        {
+            line = Console.ReadLine();
+            int[] numbers = line.Split(" ");
+            int NumberOfSolutions = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    NumberOfSolutions++;
+                }
+                if (NumberOfSolutions >= 2)
+                {
+                    sumOfSolutions++;
+                }
+            }
+            n--;
+        }
+        Console.WriteLine(sumOfSolutions);
+
+        Console.ReadLine();
+    }
+}
+}
